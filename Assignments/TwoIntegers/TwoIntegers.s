ac73dacaa4ab04d357a3ab7b444a594c3e1438b3
javierborja95 <javierborja95@gmail.com>
2017-10-09 19:56:04 -0700

    two integers hw

--- Assignments/TwoIntegers/TwoIntegers.s
+++ Assignments/TwoIntegers/TwoIntegers.s
@@ -20,59 +20,56 @@ main:
 	//Input Data
 	ldr r0, = instruct1 //Load first instruction
 	bl printf	    //Display first instruction
-	ldr r0, = spec 1    //Load single format specifier 
+	ldr r0, = spec1     //Load single format specifier 
 	ldr r1, = num1      //r1 points to first number
 	bl scanf            //Call to scanf
 	ldr r0, = instruct2 //Load second instruction
 	bl printf	    //Display second instruction
-	ldr r0, = spec 1    //Load single format specifier 
+	ldr r0, = spec1     //Load single format specifier 
 	ldr r1, = num2      //r1 points to second number
 	bl scanf            //Call to scanf
 
 	//Process Data
-	ldr r0 = num1       //r0 points to first number
+	ldr r0, = num1      //r0 points to first number
 	ldr r0, [r0]        //r0 contains value of first number
-	ldr r1 = num2       //r1 points to first number
+	ldr r1, = num2      //r1 points to first number
 	ldr r1, [r1]        //r1 contains value of first number
 	ldr r2, = sum       //r2 points to sum
 	ldr r3, = diff	    //r3 points to difference
 	ldr r4, = prod      //r4 points to product
 	add r5, r0, r1      //r5=num1+num2
-	str r2, r5          //Store result to sum label
-	sub r5, r1, r0      //r5=num2-num1
-	str r3, r5          //Store result to difference label
+	str r5, [r2]        //Store result to sum label
+	sub r5, r0, r1      //r5=num2-num1
+	str r5, [r3]        //Store result to difference label
 	mul r5, r0, r1      //r5=num1*num2
-	str r4, r5          //Store result to product label
+	str r5, [r4]        //Store result to product label
 
 	//Output Data
-	ldr r0, = spec3     //Load triple format specifier
-	ldr r1, = out1      //Load first output
-	ldr r2, = num1      //r2 points to first number
-	ldr r2, [r2]        //r2 contains value of first number
-	ldr r3, = num2      //r3 points to second number
-	ldr r3, [r3]        //r3 contains value of second number
-	ldr r4, = sum       //r4 points to the sum
-	ldr r4, [r4]        //r4 contains value of sum
+	ldr r0, = out1	    //Load first output
+	ldr r1, = num1      //r1 points to first number
+	ldr r2, = num2      //r2 points second number
+	ldr r3, = sum       //r3 points to sum
+	ldr r1, [r1]        //r1 contains value of first number
+	ldr r2, [r2]        //r2 contains value of second number
+	ldr r3, [r3]        //r3 contains value of sum
 	bl printf           //Display sum string
 
-	ldr r0, = spec3     //Load triple format specifier
-	ldr r1, = out2      //Load second output
-	ldr r2, = num1      //r2 points to first number
-	ldr r2, [r2]        //r2 contains value of first number
-	ldr r3, = num2      //r3 points to second number
-	ldr r3, [r3]        //r3 contains value of second number
-	ldr r4, = diff      //r4 points to the difference
-	ldr r4, [r4]        //r4 contains value of difference
+	ldr r0, = out2      //Load second output
+	ldr r1, = num1      //r1 points to first number
+	ldr r2, = num2      //r2 points to second number
+	ldr r3, = diff      //r3 points to difference
+	ldr r1, [r1]        //r1 contains value of first number
+	ldr r2, [r2]        //r2 contains value of second number
+	ldr r3, [r3]        //r3 contains value of difference
 	bl printf           //Display difference string
 
-	ldr r0, = spec3     //Load triple format specifier
-	ldr r1, = out3      //Load third output
-	ldr r2, = num1      //r2 points to first number
-	ldr r2, [r2]        //r2 contains value of first number
-	ldr r3, = num2      //r3 points to second number
-	ldr r3, [r3]        //r3 contains value of second number
-	ldr r4, = prod      //r4 points to the product
-	ldr r4, [r4]        //r4 contains value of product
+	ldr r0, = out3      //Load third output
+	ldr r1, = num1      //r1 points to first number
+	ldr r2, = num2      //r2 points to second  number
+	ldr r3, = prod      //r3 points to product
+	ldr r1, [r1]        //r1 contains value of first number
+	ldr r2, [r2]        //r2 contains value of second number
+	ldr r3, [r3]        //r3 contains value of product
 	bl printf           //Display product string
 	
 
